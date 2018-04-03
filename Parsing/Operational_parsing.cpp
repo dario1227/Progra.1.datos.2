6d9b4a4c5e1bf2ff25e2663ccafcab5dc9dde82e
kenneth <khken21@gmail.com>
2018-04-02 21:06:39 -0600

    Implementacion
    Se esta anadiendo el parseo de una operacion, se hicieron pruebas

--- Parsing/Operational_parsing.cpp
+++ Parsing/Operational_parsing.cpp
@@ -5,7 +5,9 @@
 #include <json-c/json_object.h>
 #include "Operational_parsing.h"
 #include "exprtk.hpp"
-RAM* Operational_parsing::table_values = nullptr;
+#include "../GUI/Interfaz.h"
+
+Interfaz* Operational_parsing::interface = nullptr;
 json_object* Operational_parsing::object= nullptr;
 bool Operational_parsing::parse(QString operation,const char* type,json_object* objeto) {
     if(type=="int"){
@@ -23,7 +25,28 @@ bool Operational_parsing::parse(QString operation,const char* type,json_object* 
     }
 }
 bool Operational_parsing::parse_int(QString operation, const char *tipo) {
-    int toAdd = 0;
+    QString string_to_parse ;
+    operation=reconstruct_without_space(&operation);
+    int index = 0;
+    while(index<operation.length()){
+        QString str;
+        while((operation[index]!= '+'||'/'||'*'||'-'||'%')&&index<operation.length()){
+            str.append(operation[index]);
+            index++;
+        }
+        if(!contains_alphabet(str,tipo)){
+            QString string_saica = get_var_value<int>(str, tipo);
+            if(string_saica=="Error"){
+                return false;
+            }
+            string_to_parse.append(string_saica);
+        }
+        else{
+            string_to_parse.append(str);
+        }
+        string_to_parse.append(operation[index]);
+        index++;
+    }
 
 }
 bool Operational_parsing::parse_floar(QString operation, const char *tipo) {
@@ -34,7 +57,7 @@ bool Operational_parsing::parse_long(QString operation, const char *tipo) {
 }
 bool Operational_parsing::parse_double(QString operation, const char *tipo) {}
 
-bool Operational_parsing::contains_alphabet(QString str,char* varType) {
+bool Operational_parsing::contains_alphabet(QString str,const char* varType) {
     if (varType=="float"||varType=="double"){
         bool x;
         str.toDouble(&x);
@@ -56,5 +79,27 @@ QString Operational_parsing::reconstruct_without_space(QString* str) {
         index++;
     }
     return str2;
+
+}
+template <typename T>
+QString Operational_parsing::get_var_value(QString variable, const char *string) {
+    int index = 0;
+    while(index<200){
+        if((string) == interface->getCell(1, index)){
+            break;
+        }
+        index++;
+    }
+    if (index==200){
+        return "ERROR";
+    }
+    if(interface->getCell(0,index)=="int"||"double"||"long"||"float"){
+        return interface->getCell(2,index);
+    }
+    else{
+
+        return "ERROR";
+    }
+
 
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include <QApplication>
 #include "GUI/Interfaz.h"
+#include "Parsing/exprtk.hpp"
 using namespace std;
 Interfaz* GUI;
+template <typename T>
+void trig_function()
+{
+    typedef exprtk::symbol_table<T> symbol_table_t;
+    typedef exprtk::expression<T>     expression_t;
+    typedef exprtk::parser<T>             parser_t;
+
+    std::string expression_string = "1+2+3";
+
+    T x;
+
+    symbol_table_t symbol_table;
+//    symbol_table.add_variable("x",x);
+//    symbol_table.add_constants();
+
+    expression_t expression;
+    expression.register_symbol_table(symbol_table);
+
+    parser_t parser;
+    parser.compile(expression_string,expression);
+
+    for (x = T(-5); x <= T(+5); x += T(0.001))
+    {
+        T y = expression.value();
+        cout<<y<<"\n";
+      //  printf("%19.15f\t%19.15f\n",x,y);
+    }
+}
+
 int main(int argc, char **argv) {
+    trig_function<double >();
     QApplication app(argc, argv);
      GUI= new Interfaz();
      QString str="34+34";
