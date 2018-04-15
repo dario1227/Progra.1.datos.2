5a808b878c45b728059fefac8e419b05eaa05f2d
kenneth <khken21@gmail.com>
2018-04-15 13:44:11 -0600

    YAAAAAAAAAAA
    Ya se supone termine eso y estoy hecho velga

--- Parsing/Syntax_analysis.h
+++ Parsing/Syntax_analysis.h
@@ -11,7 +11,9 @@
 class Syntax_analysis {
 public:
     int actual_line = 0;
-    int scope_level=0;
+    static int scope_level=0;
+    static int howmanyScopes = 0;
+    static QString  string =  QString("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     json_object* object = json_object_new_object();
 
     bool syntax_analysis(QString line, int n_line);
